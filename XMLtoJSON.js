33fa19b785d3efb0cc5a3d67aed452df716a8569
mattlag <mattlag@gmail.com>
2014-08-11 21:48:17 -0700

    somewhat working, attributes needs parsing

--- XMLtoJSON.js
+++ XMLtoJSON.js
@@ -3,9 +3,16 @@ String.prototype.ss = String.prototype.substring;
 String.prototype.io = String.prototype.indexOf;
 String.prototype.ca = String.prototype.charAt;
 
+var callcount = 0;
+
 function parseXMLtag (tag) {
+	if(callcount > 1000) { return {}; } else { callcount++; }
+
 	console.log('\nTAG\n' + tag.ss(0, 20) + '...');
 	tag = tag.replace(/(\r\n|\n|\r)/gm,"");
+
+	if(tag === '') return {};
+
 	while(tag.ca(0) !== '<') tag = tag.ss(1);
 
 	if(tag.ca(1) === '!' || tag.ca(1) === '?') {
@@ -28,7 +35,82 @@ function parseXMLtag (tag) {
 	return result;
 }
 
+function parseXMLcontent (con) {
+	if(callcount > 1000) { return {}; } else { callcount++; }
+	
+	while(con.ca(0) === ' ') con = con.ss(1);
+	console.log('\nCONTENT');
+	console.log(con);
+
+	if(con.ca(0) !== '<') return con;
+
+	var result = [];
+	var startpos = 0;
+	var endpos = 0;
+	var slashpos = 0;
+	var gtpos = 0;
+	var tagname = '';
+	var tagname_endpos = 0;
+	var trytimes = 0;
+
+	while(true){
+		console.log('\ttrytimes: ' + trytimes);
+
+		// remove whitespace
+		while(con.ca(0) === ' ') con = con.ss(1);
+		
+		// remove comments
+		while(con.ss(0,4) === '<!--'){
+			con = con.ss(con.io('-->')+3);
+			console.log('\tremoved comment, con is now ' + con);
+		}
+
+		// remove whitespace
+		while(con.ca(0) === ' ') con = con.ss(1);
+
+		console.log('con is ' + con + ' !!con is ' + !!con + ' typeof con is ' + typeof con);
+		
+		if(con === '' || trytimes > 10){
+			console.log('\tcontent is empty string, returning ' + JSON.stringify(result));
+			return result;
+		}
+
+		// get name
+		gtpos = con.io('>');
+		slashpos = con.io('/');
+		tagname_endpos = Math.min(con.io(' '), Math.min(gtpos, slashpos));
+		tagname = con.ss(startpos+1, tagname_endpos);
+		console.log('\ttag name: ' + tagname);
+
+		// self closing?
+		if(slashpos === (gtpos-1)){
+			result.push({
+				'name' : tagname,
+				'attributes' : parseXMLattributes(con.ss(tagname_endpos, slashpos)),
+				'content' : false
+			});
+			con = con.ss(gtpos+1);
+		} else {
+			endpos = con.io('</'+tagname+'>') + 3 + tagname.length;
+			result.push(parseXMLtag(con.ss(startpos, endpos)));
+			con = con.ss(endpos);
+		}
+
+		while(con.ca(0) === ' ') con = con.ss(1);
+		if(con === '' || trytimes > 10){
+			console.log('\tdone with all tags in content, returning ' + JSON.stringify(result));
+			return result;
+		}
+		startpos = 0;
+		endpos = 0;
+	}
+
+	return result;
+}
+
 function parseXMLattributes (attr) {
+	if(callcount > 1000) { return {}; } else { callcount++; }
+	
 	console.log('\nATTRIBUTES');
 
 	while(attr.io(' =') > -1) attr = attr.replace(' =', '=');
@@ -55,37 +137,6 @@ function parseXMLattributes (attr) {
 				'value' : pair[1].ss(1, pair[1].length-1)
 			});
 		}
-	}
-
-	return result;
-}
-
-function parseXMLcontent (con) {
-	while(con.ca(0) === ' ') con = con.ss(1);
-	console.log('\nCONTENT');
-	console.log(con);
-
-	if(con.ca(0) !== '<') return con;
-
-	var result = [];
-	var startpos = 0;
-	var endpos = 0;
-	var tagname = '';
-	var tagname_endpos = 0;
-	var trytimes = 0;
-
-	while(true){
-		console.log('\ttrytimes: ' + trytimes);
-		tagname_endpos = Math.min(con.io(' '), Math.min(con.io('/'), con.io('>')));
-		tagname = con.ss(startpos+1, tagname_endpos);
-		console.log('\ttag name: ' + tagname);
-		endpos = con.io('</'+tagname+'>') + 3 + tagname.length;
-		result.push(parseXMLtag(con.ss(startpos, endpos)));
-
-		con = con.ss(endpos);
-		if(con === '' || trytimes > 10) return result;
-		startpos = 0;
-		endpos = 0;
 	}
 
 	return result;
